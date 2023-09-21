3cdc8be5c96bb0848b92b033ddb1021bddf8d949
codesche <codesche@gmail.com>
2023-09-21 18:32:37 +0900

    feat: MemberService 구성

--- src/main/java/service/MemberServiceImpl.java
+++ src/main/java/service/MemberServiceImpl.java
@@ -1,14 +1,21 @@
 package service;
 
+import dto.MemberSaveDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import repository.MemberRepository;
 
 @Service
 @RequiredArgsConstructor
-public class MemberServiceImpl {
+public class MemberServiceImpl implements MemberService {
 
     // Repository 생성자 주입.
     private final MemberRepository mr;
+
+    @Override
+    public Long save(MemberSaveDTO memberSaveDTO) {
+        return null;
+    }
 
     // 회원가입 정보 저장
 
